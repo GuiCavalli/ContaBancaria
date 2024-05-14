b31c613ed72655b212026984c23bf6f7c7109f3f
GuiCavalli <160363168+GuiCavalli@users.noreply.github.com>
2024-05-13 22:12:07 -0300

    Add files via upload

---
+++ ContaBancaria/src/main/java/br/unipar/ContaPoupanca.java
@@ -0,0 +1,18 @@
+package br.unipar;
+
+public class ContaPoupanca extends HerancaContaBancaria {
+    private static  final double TAXA_SAQUE = 5.0;
+
+    @Override
+    public boolean saque(Double valor) {
+
+        if((saldo -5) >= valor){
+            saldo -= valor;
+            saldo -= TAXA_SAQUE;
+            return true;
+        }
+
+        return false;
+    }
+
+}
